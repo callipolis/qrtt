419c53bad57f6b56e281fefb8486bc0cd02dbb47
Leopold W <leopold@lwco.com>
2021-03-13 00:26:51 -0500

    Update Bollinger Bands Formula
    Create _bbands_calc() function to calculate all the necessary values for the technical indicator. Update bbands() and percent_b() to utilize the new _bbands_calc() function. So code for basic calculations only appear once.

--- qrtt/technical/boll.py
+++ qrtt/technical/boll.py
@@ -9,32 +9,22 @@ Lower Band – 20 Day Simple Moving Average - (Standard Deviation x 2)
 """
 
 
-def bbands(ohlcv, period=20, number_of_std=2, ohlcv_series="close"):
-    """
-    Outcomes have been validated using data from Bloomberg and TradingView.
-    """
+def _bbands_calc(ohlcv, period=20, number_of_std=2, ohlcv_series="close"):
     _ohlcv = ohlcv[[ohlcv_series]].copy(deep=True)
     _ohlcv["mid_band"] = _ohlcv[ohlcv_series].rolling(window=period, min_periods=period).mean()
-
     _ohlcv["std"] = _ohlcv[ohlcv_series].rolling(window=period, min_periods=period).std(ddof=0)
     _ohlcv["lower_band"] = _ohlcv["mid_band"] - number_of_std * _ohlcv["std"]
     _ohlcv["upper_band"] = _ohlcv["mid_band"] + number_of_std * _ohlcv["std"]
+    return _ohlcv
 
+
+def bbands(ohlcv, period=20, number_of_std=2, ohlcv_series="close"):
+    _ohlcv = _bbands_calc(ohlcv, period, number_of_std, ohlcv_series)
     return _ohlcv[["lower_band", "mid_band", "upper_band"]]
 
-#
-def pct_bbands(ohlcv, period=20, number_of_std=2, ohlcv_series="close"):
-    """
-    Outcomes have been validated using data from Bloomberg and TradingView.
-    """
-    _ohlcv = ohlcv[[ohlcv_series]].copy(deep=True)
-    _ohlcv["mid_band"] = _ohlcv[ohlcv_series].rolling(window=period, min_periods=period).mean()
 
-    _ohlcv["std"] = _ohlcv[ohlcv_series].rolling(window=period, min_periods=period).std(ddof=0)
-    _ohlcv["lower_band"] = _ohlcv["mid_band"] - number_of_std * _ohlcv["std"]
-    _ohlcv["upper_band"] = _ohlcv["mid_band"] + number_of_std * _ohlcv["std"]
-
+def percent_b(ohlcv, period=20, number_of_std=2, ohlcv_series="close"):
+    _ohlcv = _bbands_calc(ohlcv, period, number_of_std, ohlcv_series)
     indicator_values = (_ohlcv[ohlcv_series] - _ohlcv['lower_band']) / (_ohlcv['upper_band'] - _ohlcv['lower_band'])
-
     return indicator_values
 
